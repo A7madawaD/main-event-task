24a0c5bf9aae082a9b2cada455c5be64929a1fba
Ahmed Awad <ahmedawed878@gmail.com>
2026-08-01 22:21:04 +0300

    Improve website design and add new book sections

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// زر "اقرأ الاقتباس"
+document.querySelectorAll(".flip-btn").forEach((button) => {
+  button.addEventListener("click", function () {
+    const book = this.closest(".book");
+    book.querySelector(".book-inner").style.transform = "rotateY(180deg)";
+  });
+});
+
+// زر "رجوع"
+document.querySelectorAll(".back-btn").forEach((button) => {
+  button.addEventListener("click", function () {
+    const book = this.closest(".book");
+    book.querySelector(".book-inner").style.transform = "rotateY(0deg)";
+  });
+});
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+// إظهار وإخفاء الزر
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+// الرجوع لأعلى الصفحة
+scrollBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
